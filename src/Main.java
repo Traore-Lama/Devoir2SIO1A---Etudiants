0b722806ebdfb234e3b2ffb4adb72ca2b28549f5
Lama <traorelama997@gmail.com>
2025-12-14 17:32:02 +0100

    Correction Lama

--- src/Main.java
+++ src/Main.java
@@ -23,59 +23,77 @@ public class Main {
                 case 1 :
                     // Ne pas effacer cette ligne
                     int[] tabExo1 = new int[5];
+                    Random rand = new Random();
                     int somme = 0;
-                    int valeur = 0;
-                    int faces = 0;
+                    boolean contientUn = false;
 
                     // A vous de jouer
-                    System.out.println("Contenue du Tableau : ");
+                    System.out.print("Contenu du Tableau : ");
+
                     for (int i = 0; i < tabExo1.length; i++)
                     {
+                        // Génération d'un dé entre 1 et 6
+                        tabExo1[i] = rand.nextInt(6) + 1;
+
                         System.out.print(tabExo1[i] + " - ");
+
                         somme += tabExo1[i];
-                        // boucle allant de 1 a 6 ?
-                        for(int j = 1; j < 6; j++)
+
+                        if (tabExo1[i] == 1)
                         {
-                            faces = valeur;
+                            contientUn = true;
                         }
                     }
+
                     System.out.println();
                     System.out.println("Somme du Tableau : " + somme);
-                    // victoire si le resultats des faces est >= 18 sinon perdue
-                    if (faces >= 18)
+
+                    if (somme >= 18 && !contientUn)
                     {
-                        System.out.println("Partie gagnee");
-                    }else
-                    {
-                        System.out.print("Partie perdue");
+                        System.out.println("Partie gagnée");
                     }
-                    System.out.println();
+                    else
+                    {
+                        System.out.println("Partie perdue");
+                    }
 
                     break;
                 case 2 :
                     // Ne pas effacer cette ligne
                     int[] scoresPossibles = new int[]{0,15,30,40};
-                    int scoreJoueur = 0;
+                    int pointsA = 0;
+                    int pointsB = 0;
 
                     // Voici des exemples de parties que vous pourrez tester
-                    String[] partie = new String[]{"A","B","B","A","A","A"};
+                     String[] partie = new String[]{"A","B","B","A","A","A"};
                     //String[] partie = new String[]{"A","B","B","A","A","B","A","B","B","A","B","B"};
                     //String[] partie = new String[]{"B","B","B","B"};
                     //String[] partie = new String[]{"B","B","A","A","A","B","A","B","A","A"};
 
                     // A vous de jouer
-                        // parcour le tableau des scores
-                         for (int i = 0; i < scoresPossibles.length; i++)
-                         {
-                             // parcour pour savoir si c'est le joueur A ou B
-                             for (int j = 0; j < partie.length; j++)
-                             {
+                    for (int i = 0; i < partie.length; i++) {
 
-                             }
-                         }
-                         //Afficher la chaine de caractere Partie
-                         //Comparer le score des joueur pour donner le vainqueur
-                    break;
+                        if (partie[i].equals("A")) { // pour comparer un strings on utilise toujours equals (si l'indice est egal a "A")
+                            pointsA++;
+                        } else {
+                            pointsB++;
+                        }
+
+                        int scoreA = Math.min(pointsA, 3); // pointsA ne peut pas depasser l'index 3 = 40
+                        int scoreB = Math.min(pointsB, 3); // pointsB ne peut pas depasser l'index 3 = 40
+
+                        System.out.println(scoresPossibles[scoreA] + " / " + scoresPossibles[scoreB]);
+
+                        if (pointsA >= 4 && pointsA > pointsB) {
+                            System.out.println("Le joueur A a gagné");
+                            break;
+                        }
+                        else if (pointsB >= 4 && pointsB > pointsA)
+                        {
+                            System.out.println("Le joueur B a gagné");
+                            break;
+                        }
+                    }
                 case 0 :
                     System.out.println("FIN DU PROGRAMME");
                     break;
